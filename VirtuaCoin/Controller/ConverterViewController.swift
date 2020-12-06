36d2dc5f21af9d672c316cabbe3be972620eaa6e
xybiz <92678045+xybiz@users.noreply.github.com>
2020-12-06 11:59:30 +0000

    Update AppDelegate.m

--- VirtuaCoin/Controller/ConverterViewController.swift
+++ VirtuaCoin/Controller/ConverterViewController.swift
@@ -1,2 +1,16 @@
 
 //
+//  ViewController.swift
+//  VirtuaCoin
+//
+//  Created by p.montes.vallespir on 18/10/2020.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var coinPicker: UIPickerView!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coins: [String] = [String]()
