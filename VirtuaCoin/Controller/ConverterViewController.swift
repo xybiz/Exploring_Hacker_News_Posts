9b93019e59d5548deffd63628bba65993db79ef5
xybiz <92678045+xybiz@users.noreply.github.com>
2020-12-09 15:24:21 +0000

    cifs: Allow passwords which begin with a delimitor

--- VirtuaCoin/Controller/ConverterViewController.swift
+++ VirtuaCoin/Controller/ConverterViewController.swift
@@ -14,3 +14,15 @@ class ConverterViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     @IBOutlet weak var currencyPicker: UIPickerView!
     
     var coins: [String] = [String]()
+    var currencies: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.coinPicker.delegate = self
+        self.coinPicker.dataSource = self
+        self.currencyPicker.delegate = self
+        self.currencyPicker.dataSource = self
+        
+        // Input the data into the array
+        coins = ["Coin 1", "Coin 2", "Coin 3", "Coin 4"]
