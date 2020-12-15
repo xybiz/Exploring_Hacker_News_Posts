e2e3d8621d9e833bc0c06f5c77504f1fa9ba2727
xybiz <92678045+xybiz@users.noreply.github.com>
2020-12-15 15:40:49 +0000

    Changed license.

--- VirtuaCoin/SceneDelegate.swift
+++ VirtuaCoin/SceneDelegate.swift
@@ -1,3 +1,13 @@
 
 //
 //  SceneDelegate.swift
+//  VirtuaCoin
+//
+//  Created by p.montes.vallespir on 18/10/2020.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
