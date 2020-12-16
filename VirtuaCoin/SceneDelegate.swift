4bc6aa7795bba205e9353f3c811e6b8e3b2ed5e5
xybiz <92678045+xybiz@users.noreply.github.com>
2020-12-16 22:52:57 +0000

    (no commit message)

--- VirtuaCoin/SceneDelegate.swift
+++ VirtuaCoin/SceneDelegate.swift
@@ -26,3 +26,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
