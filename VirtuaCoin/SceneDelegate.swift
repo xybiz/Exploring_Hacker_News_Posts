c2cdb4ed7ae44be7bec653ebf64a084df39f5cda
xybiz <92678045+xybiz@users.noreply.github.com>
2020-12-18 06:36:18 +0000

    36316-redesign

--- VirtuaCoin/SceneDelegate.swift
+++ VirtuaCoin/SceneDelegate.swift
@@ -34,3 +34,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+    }
